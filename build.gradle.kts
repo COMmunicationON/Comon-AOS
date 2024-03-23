96bc2c8a57e1bb3332f7c79d07800793ef15e851
Imagine-Choi <sang8408@naver.com>
2024-03-23 14:13:18 +0900

    Fix: toml plugin 추가 및 gradle 오류 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.oss.licenses.plugin)
+    }
+}
+
 plugins {
-//    alias(libs.plugins.androidApplication) apply false
-//    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-//    alias(libs.plugins.androidLibrary) apply false
-//    alias(libs.plugins.kotlinxSerialization) apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.kotlinxSerialization) apply false
 }
